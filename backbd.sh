8c45ed2ebc8a7fda0176c2a28ca971a928487c4b
uelei <github@uelei.com>
2014-07-06 13:04:29 -0300

    ok

--- backbd.sh
+++ backbd.sh
@@ -9,19 +9,19 @@ cd ..
 mkdir $PEN'/backup_bd/'$DATA
 cd $PEN'/backup_bd/'$DATA
 printf "realizando backup tb_pb"
-mysqldump -u root -puc3r2l -x -e -B tb_pb | gzip > backup_tbpb-$DATA.sql.gz
+mysqldump -u root -pPASSWORD -x -e -B tb_pb | gzip > backup_tbpb-$DATA.sql.gz
 echo " ok " 
 printf "realizando backup pbbd"
-mysqldump -u root -puc3r2l -x -e -B pbbd | gzip > backup_pbbd-$DATA.sql.gz
+mysqldump -u root -pPASSWORD -x -e -B pbbd | gzip > backup_pbbd-$DATA.sql.gz
 echo " ok"
 printf "realizando backup matrix"
-mysqldump -u root -puc3r2l -x -e -B matrix | gzip > backup_matrix-$DATA.sql.gz
+mysqldump -u root -pPASSWORD -x -e -B matrix | gzip > backup_matrix-$DATA.sql.gz
 echo " ok"
 printf "realizando backup tbpb001"
-mysqldump -u root -puc3r2l -x -e -B tbpb001 | gzip > backup_tbpb001-$DATA.sql.gz
+mysqldump -u root -pPASSWORD -x -e -B tbpb001 | gzip > backup_tbpb001-$DATA.sql.gz
 echo " ok"
 printf "realizando backup dbfmy"
-mysqldump -u root -puc3r2l -x -e -B dbfmy | gzip > backup_dbfmy-$DATA.sql.gz
+mysqldump -u root -pPASSWORD -x -e -B dbfmy | gzip > backup_dbfmy-$DATA.sql.gz
 echo " ok"
 cd $PEN'/backup_bd'
 echo "compactando SQLs"
